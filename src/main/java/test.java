1b0b5a9ccc07abbe571cf23158cf4f12d29d2f5e
Drusilio <andrey.shlyahtovich@gmail.com>
2021-10-16 20:59:21 +0300

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+    public static void main (String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Хаю");
+        list.add("хай");
+        list.add("с вами");
+        list.add("сверхзвуковой комбайн");
+
+        for()
+    }
+}
